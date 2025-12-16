c885d22cb06b1b504251c856f419675804d297bc
Caio Csermak <ccaiocsermak@gmail.com>
2025-12-16 13:09:57 -0300

    Inicialização do projeto Spring

---
+++ src/main/java/org/example/ClientesApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApplication{
+    //Básico para uma aplicação springboot
+    public static void main(String[] args){
+        SpringApplication.run(ClientesApplication.class, args);
+    }
+}
